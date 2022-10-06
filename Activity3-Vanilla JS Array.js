4b8bfdfb8af9626bdc61a3675b56072171a942a3
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-10-06 22:57:17 +0100

    Started

---
+++ Activity3-Vanilla JS Array.js
@@ -0,0 +1,17 @@
+*/
+Activity 3 - Vanilla JS Arrays
+Write a JavaScript program to find the maximum and minimum value of an array.
+
+Test Data:
+
+Sample Array:
+
+[25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+Expected Output:
+
+Original Array: [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+Maximum value for the above array = 77
+
+Minimum value for the above array = 14 /*
