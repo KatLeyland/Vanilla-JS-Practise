03e7a62a94db45e367235a581ea2ffdd1ca87730
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-10-07 09:52:17 +0100

    Still needs to be commented

--- Activity3-Vanilla JS Array.js
+++ Activity3-Vanilla JS Array.js
@@ -1,4 +1,4 @@
-*/
+/*
 Activity 3 - Vanilla JS Arrays
 Write a JavaScript program to find the maximum and minimum value of an array.
 
@@ -14,4 +14,19 @@ Original Array: [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
 
 Maximum value for the above array = 77
 
-Minimum value for the above array = 14 /*
+Minimum value for the above array = 14 */
+
+let arr = [25, 14, 56, 15, 36, 56, 77, 18, 29, 49];
+console.log("Original array : " + arr);
+let largestNumber = 0;
+let smallestNumber = 99999999999999999;
+for (let i = 0; i<arr.length; i++){
+    if (arr[i] > largestNumber) {
+        largestNumber=arr[i];
+    }
+    if (arr[i] < smallestNumber) {
+        smallestNumber=arr[i];
+    }
+}
+console.log("Maximum value for the above array = " + largestNumber);
+console.log("Miniimum value for the above array = " + smallestNumber);
